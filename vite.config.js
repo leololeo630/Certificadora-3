b31c3f312629e88ca7e7b29149afa43f6dcd9b65
Paulo Mitsi <paulomitsi.contato@hotmail.com>
2025-05-25 18:01:27 -0300

    integração backend com front
    adicionado configurações no vite.config para o backend
    
    feito um arquivo server.js que lida com a criação do servidor e criação das tabelas. Nesse arquivo, é lidado com as requisições POST e GET.
    
    instalado
    - express: Framework para criar o servidor e as rotas da API.
    - sqlite3: Driver para interagir com o SQLite.
    - cors: Middleware para habilitar o Cross-Origin Resource Sharing (permite que o frontend acesse o backend no localhost:3001).

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        port: 5173, // Porta padrão do Vite
+        proxy: {
+            // Redireciona requisições que começam com /api para o seu servidor backend
+            '/api': {
+                target: 'http://localhost:3001', // URL do servidor Node.js
+                changeOrigin: true, // Necessário para virtual hosted sites
+                secure: false, // Se o seu backend não usa HTTPS em desenvolvimento
+            },
+        },
+    },
+});
