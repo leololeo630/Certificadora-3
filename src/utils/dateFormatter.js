623baa2820eb2caa781cda4e0da7ca8894a1e49f
mateuspuertas <145804402+mateuspuertas@users.noreply.github.com>
2025-06-01 19:17:26 -0300

    Create dateFormatter.js

---
+++ src/utils/dateFormatter.js
@@ -0,0 +1,5 @@
+// Formata datas para exibição
+export const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(dateString).toLocaleDateString('pt-BR', options);
+};
